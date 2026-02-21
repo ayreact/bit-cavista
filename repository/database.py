99a52585265236c1e86c52503753d98d7a2c210d
HenryTech12 <fakorodehenry@gmail.com>
2026-02-21 17:58:08 +0100

    implemented supabase for postgresql cloud database

--- repository/database.py
+++ repository/database.py
@@ -1,10 +1,15 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./cardiotwin.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SUPABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@db.cgmlwqgnaqszhqljzbid.supabase.co:5432/postgres"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
